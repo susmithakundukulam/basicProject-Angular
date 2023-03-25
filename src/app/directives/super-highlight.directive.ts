2f5b65c8ccfceeb6376d650111ffc92ecee8fc3d
Susmitha Kundukulam <susmithakundukulam@gmail.com>
2023-03-25 16:01:03 +0530

    custom durective

--- src/app/directives/super-highlight.directive.ts
+++ src/app/directives/super-highlight.directive.ts
@@ -1,10 +1,21 @@
-import { Directive } from '@angular/core';
+import { Directive, HostBinding, HostListener, Input } from '@angular/core';
 
 @Directive({
   selector: '[appSuperHighlight]'
 })
 export class SuperHighlightDirective {
 
+  @HostBinding('style.backgroundColor') backgroundColor: String;
+  @Input() color: String;
+
   constructor() { }
+
+  @HostListener('mouseenter') mouseovers(data : Event){
+    this.backgroundColor = this.color;
+  }
+
+  @HostListener('mouseleave') mouseleave(){
+    this.backgroundColor = 'transparent';
+  }
 
 }
